edbbfbd50e571320987217f324a9d16b09752467
Sara Ferreira <saraywebst@gmail.com>
2025-02-11 21:02:16 -0300

    :construction: layout mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ export const Header = () => {
     <header className="w-full max-w-4xl p-5 bg-white/10 rounded-lg flex justify-between items-center shadow-md">
       <h1 className="text-xl font-bold">Sara Ferreira</h1>
       <nav className="flex gap-5">
-        <Link to="/" className="text-gray-300 hover:text-purple-400">
+        <Link to="#" className="text-gray-300 hover:text-purple-400">
           Sobre
         </Link>
-        <Link to="/" className="text-gray-300 hover:text-purple-400">
+        <Link to="#" className="text-gray-300 hover:text-purple-400">
           Projetos
         </Link>
-        <Link to="/" className="text-gray-300 hover:text-purple-400">
+        <Link to="#" className="text-gray-300 hover:text-purple-400">
           Contato
         </Link>
       </nav>
